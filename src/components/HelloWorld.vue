8fe9bec75ca1f5bf3d4f4ec5487cf5723ba99b84
jasnapk-hm <hmjasnapk@gmail.com>
2024-12-06 16:24:31 +0530

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h1>Login form</h1>
+    <div
+      :style="{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+
+        alignItems: 'center',
+        justifyContent: 'center',
+      }"
+    >
+      <input
+        type="text"
+        v-model="LoginForm.name"
+        placeholder="Enter your name"
+      /><br>
+
+      <input
+        type="password"
+        v-model="LoginForm.password"
+        placeholder="Enter your password"
+      /><br>
+
+      <button @click="getLogin">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      LoginForm: {
+        name: null,
+        password: null,
+      },
+    };
+  },
+  methods: {
+    getLogin() {
+      console.log("login info", this.LoginForm);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
